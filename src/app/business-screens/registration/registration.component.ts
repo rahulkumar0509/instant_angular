a3af21980bd076e66bfd044450af31e2db263748
rahul kumar <rahul.kumar0509@outlook.com>
2023-02-08 23:07:38 -0500

    adding upload & download json

--- src/app/business-screens/registration/registration.component.ts
+++ src/app/business-screens/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-registration',
@@ -12,89 +13,91 @@ export class RegistrationComponent implements OnInit {
   submitted = false;
   data ={};
   newForm = false;
+  oldForm = true;
   fileToUpload: any;
   uploadedJson:any;
+  template = {
+    "screenName": "registrationForm",
+    "controls": [
+      {
+        "name": "username",
+        "component": "INPUT",
+        "mandatory": true,
+        "label": "Username",
+        "placeholder": "Enter username",
+        "defaultValue": "rahul",
+        "defaultValidation": "SPECIAL_CHAR_PROHIBITED"
+      },
+      {
+        "name": "Email",
+        "component": "INPUT",
+        "mandatory": true,
+        "label": "Email id",
+        "placeholder": "Enter email id",
+        "defaultValue": " rahul@instant.com",
+        "defaultValidation": "EMAIL"
+      },
+      {
+        "name": "password",
+        "component": "INPUT",
+        "mandatory": true,
+        "label": "Password",
+        "placeholder": "Enter password",
+        "defaultValue": "",
+        "defaultValidation": ""
+      },
+      {
+        "name": "confirmPassword",
+        "component": "INPUT",
+        "mandatory": true,
+        "label": "Confirm password ",
+        "placeholder": "Enter confirm password",
+        "defaultValue": "",
+        "defaultValidation": ""
+      },
+      {
+        "name": "age",
+        "component": "INPUT",
+        "mandatory": true,
+        "label": "Age ",
+        "placeholder": "Enter age",
+        "defaultValue": "22",
+        "defaultValidation": ""
+      },
+      {
+        "name": "sex",
+        "component": "INPUT",
+        "mandatory": true,
+        "label": "Sex",
+        "placeholder": "Enter sex",
+        "defaultValue": "N/A",
+        "defaultValidation": ""
+      }
+    ],
+    "header": {
+      "title": "User registration form 2",
+      "subtitle": "Fill out the basic details.",
+      "actions":[
+        {
+          "type": "SUBMIT",
+          "label": "Register",
+          "apiUrl": "/registration",
+          "buttonType": "primary"
+        },
+        {
+          "type": "RESET",
+          "label": "Reset",
+          "apiUrl": "",
+          "buttonType": "secondary"
+        }
+      ]
+    }
+  };
 
   constructor() { }
 
   ngOnInit(): void {
-    this.registrationConfig = {
-      "screenName": "registrationForm",
-      "controls": [
-        {
-          "name": "username",
-          "component": "INPUT",
-          "mandatory": true,
-          "label": "Username",
-          "placeholder": "Enter username",
-          "defaultValue": "rahul",
-          "defaultValidation": "SPECIAL_CHAR_PROHIBITED"
-        },
-        {
-          "name": "Email",
-          "component": "INPUT",
-          "mandatory": true,
-          "label": "Email id",
-          "placeholder": "Enter email id",
-          "defaultValue": " rahul@instant.com",
-          "defaultValidation": "EMAIL"
-        },
-        {
-          "name": "password",
-          "component": "INPUT",
-          "mandatory": true,
-          "label": "Password",
-          "placeholder": "Enter password",
-          "defaultValue": "",
-          "defaultValidation": ""
-        },
-        {
-          "name": "confirmPassword",
-          "component": "INPUT",
-          "mandatory": true,
-          "label": "Confirm password ",
-          "placeholder": "Enter confirm password",
-          "defaultValue": "",
-          "defaultValidation": ""
-        },
-        {
-          "name": "age",
-          "component": "INPUT",
-          "mandatory": true,
-          "label": "Age ",
-          "placeholder": "Enter age",
-          "defaultValue": "22",
-          "defaultValidation": ""
-        },
-        {
-          "name": "sex",
-          "component": "INPUT",
-          "mandatory": true,
-          "label": "Sex",
-          "placeholder": "Enter sex",
-          "defaultValue": "N/A",
-          "defaultValidation": ""
-        }
-      ],
-      "header": {
-        "title": "User registration form 2",
-        "subtitle": "Fill out the basic details.",
-        "actions":[
-          {
-            "type": "SUBMIT",
-            "label": "Register",
-            "apiUrl": "/registration",
-            "buttonType": "primary"
-          },
-          {
-            "type": "RESET",
-            "label": "Reset",
-            "apiUrl": "",
-            "buttonType": "secondary"
-          }
-        ]
-      }
-    }
+    this.registrationConfig = this.template;
     this.registrationConfig = JSON.stringify(this.registrationConfig);
   }
 
@@ -189,12 +192,18 @@ export class RegistrationComponent implements OnInit {
         ]
       },
     };
-    this.newForm = true;
+    this.newForm = !this.newForm;
+    this.oldForm = !this.oldForm;
     }
     fileReader.readAsText(this.uploadedJson);
   }
   
   handleFileInput(target: any) {
     this.uploadedJson = target.files[0];
+  }
+
+  downloadJson(){
+    let fileToSave = new Blob([JSON.stringify(this.template)], {type: 'application/json'});
+    saveAs(fileToSave, "dummy.json");
   }
 }
